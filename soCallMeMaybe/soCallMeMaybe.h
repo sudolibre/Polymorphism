445136569894f3bef8c5e74f6c6474c93db8b8fa
sudolibre <optout.breaker@gmail.com>
2016-12-13 13:37:13 -0500

    Initial Commit

---
+++ soCallMeMaybe/soCallMeMaybe.h
@@ -0,0 +1,19 @@
+//
+//  soCallMeMaybe.h
+//  soCallMeMaybe
+//
+//  Created by Jonathon Day on 12/13/16.
+//  Copyright © 2016 dayj. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for soCallMeMaybe.
+FOUNDATION_EXPORT double soCallMeMaybeVersionNumber;
+
+//! Project version string for soCallMeMaybe.
+FOUNDATION_EXPORT const unsigned char soCallMeMaybeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <soCallMeMaybe/PublicHeader.h>
+
+
